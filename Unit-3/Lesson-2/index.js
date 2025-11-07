c08e530664c6123bc478aee573a6aa3e3ef084a5
piyush-circuitstream <piyush@circuitstream.ca>
2025-11-06 21:26:28 -0700

    Added Lesson2 of Unit 3 class exercise

---
+++ Unit-3/Lesson-2/index.js
@@ -0,0 +1,32 @@
+//Objective: I want to serve the request coming to my server(localhost/127.0.0.1) with differeent HTML files 
+// for different seasons(Winter/Summer) depending on the value provided in query parameter.
+
+const http = require('http');
+const fs = require('fs');
+const qs = require('querystring');
+
+const server = http.createServer((req, res) => {
+
+    //Read the query parameter from the URL (localhost:8080/?month=5)
+    const queryString = req.url.split('?')[1];
+    const query = qs.parse(queryString); //month 5
+    const monthNumber = query.month;
+
+    const fileName = (monthNumber > 4 & monthNumber < 10) ? 'summer.html' : 'winter.html';
+
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('File not found');
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+});
+
+// Start the server and listen on the specified port (http://localhost:8080/)
+const PORT = 8080;
+const HOSTNAME = 'localhost'; // localhost or 127.0.0.1 
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
