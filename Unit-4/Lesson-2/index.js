f2b04aa1b5158d645629860e727d3291928cbd4e
piyush-circuitstream <piyush@circuitstream.ca>
2025-12-18 20:57:58 -0700

    Added demo for ejs of Unit 4 Lesson 2

---
+++ Unit-4/Lesson-2/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hello from EJS Template!', firstname: 'John', lastname: 'Doe', usecase: 'Express with EJS' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
