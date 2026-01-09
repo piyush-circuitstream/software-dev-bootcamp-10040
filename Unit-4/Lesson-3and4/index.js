7e7e1783601874f6bf3473dab53dc47554beea89
piyush-circuitstream <piyush@circuitstream.ca>
2026-01-08 20:39:03 -0700

    CRUD operation kick starter with create and update exmple

--- Unit-4/Lesson-3and4/index.js
+++ Unit-4/Lesson-3and4/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
 const booksRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', booksRouter);
+
+mongoose.connect('mongodb://localhost:27017/booksInventory')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    }).catch(err => {
+        console.error('Failed to connect to MongoDB', err);
+    });
 
 async function startServer() {
     const PORT = 3000;
--- Unit-4/Lesson-3and4/routes/books.js
+++ Unit-4/Lesson-3and4/routes/books.js
@@ -1,18 +1,25 @@
 const express = require('express');
-
+const { Book } = require('../models/books');
 const router = express.Router();
 
 // Get list of books
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     // Static books data to be rendered, but later needs to be replaced with database data
-    const books = [
-        { title: "Book Title1", isbn: "1234567", publisher: "AGS", inventory: 5, unitsSold: 2, tags: "Drama,Fiction" },
-        { title: "Book Title2", isbn: "2345671", publisher: "DPS", inventory: 7, unitsSold: 3, tags: "Thriller" },
-        { title: "Book Title3", isbn: "3456712", publisher: "DRO", inventory: 10, unitsSold: 7, tags: "Action,Fiction" },
-        { title: "Book Title4", isbn: "4567123", publisher: "CGS", inventory: 2, unitsSold: 1, tags: "Drama,Action,Sci-Fi" }
-    ];
+    // const books = [
+    //     { title: "Book Title1", isbn: "1234567", publisher: "AGS", inventory: 5, unitsSold: 2, tags: "Drama,Fiction" },
+    //     { title: "Book Title2", isbn: "2345671", publisher: "DPS", inventory: 7, unitsSold: 3, tags: "Thriller" },
+    //     { title: "Book Title3", isbn: "3456712", publisher: "DRO", inventory: 10, unitsSold: 7, tags: "Action,Fiction" },
+    //     { title: "Book Title4", isbn: "4567123", publisher: "CGS", inventory: 2, unitsSold: 1, tags: "Drama,Action,Sci-Fi" }
+    // ];
 
-    res.render('books', { books }); // Render the books.ejs template with the books data
+    const booksList = await Book.find({});
+
+    try {
+        res.render('books', { books: booksList }); // Render the books.ejs template with the books data
+    } catch (error) {
+        console.error('Error fetching books:', error);
+    }
+
 });
 
 // Display form to add a new book
@@ -21,12 +28,28 @@ router.get('/add', (req, res) => {
 });
 
 // Create a book
-router.post('/add', (req, res) => {
+router.post('/add', async (req, res) => {
+    try {
+        const tags = req.body.tags.split(',');
+        req.body.tags = tags;
+        await Book.create(req.body);
+        res.render('form', { title: 'Add a new Book', action: '/add', message: 'Book added successfully!' });
+    } catch (error) {
+        console.error('Error creating book:', error);
+        res.render('form', { title: 'Add a new Book', action: '/add', message: 'Error creating a book!' });
+    }
+
 });
 
 // Display form to edit a book
-router.get('/edit/:id', (req, res) => {
-    res.render('form', { title: 'Edit a Book', action: `/edit/${req.params.id}` }); // Render the form.ejs template for editing a book
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        res.render('form', { title: 'Edit a Book', action: `/edit/${req.params.id}`, book }); // Render the form.ejs template for editing a book
+    } catch (error) {
+        console.error('Error fetching book for edit:', error);
+    }
+
 });
 
 // Update a book
