e5ff87af2c3ffe9b4726278da714da7f5d1370fd
piyush-circuitstream <piyush@circuitstream.ca>
2026-02-03 21:58:16 -0700

    Fibonnaci solution

--- Unit-5/Lesson-2/fibonacci.js
+++ Unit-5/Lesson-2/fibonacci.js
@@ -1,6 +1,19 @@
 function fibonacci(n) {
-
+    if (n <= 1) {
+        return n; // base case: return n itself for 0 or 1
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2); // recursive case
 }
+
+function getFibonacciSequence(x) {
+    let sequence = [];
+    for (let i = 0; i < x; i++) {
+        sequence.push(fibonacci(i)); // push the fibonacci number at index i
+    }
+    return sequence;
+}
+
 
 let n = 5;
 console.log(`Fibonacci of ${n} is ${fibonacci(n)}`);
+console.log(`Fibonacci sequence up to ${n} terms:`, getFibonacciSequence(n));
