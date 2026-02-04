d39f16a91d7cb5a90c761be251b36e021693d770
piyush-circuitstream <piyush@circuitstream.ca>
2026-02-03 20:53:41 -0700

    Linkedlist

---
+++ Unit-5/Lesson-2/linkedlist.js
@@ -0,0 +1,41 @@
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+
+    size() {
+        let count = 0;
+        let current = this.head;
+        while (current) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    clear() {
+        this.head = null;
+    }
+}
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+const node1 = new Node(10);
+const node2 = new Node(20);
+const node3 = new Node(30);
+
+
+node1.next = node2;
+node2.next = node3;
+
+const linkedlist = new LinkedList(node1);
+
+console.log(linkedlist);
+console.log("Size of linked list:", linkedlist.size());
+linkedlist.clear();
+console.log("Linked list after clearing:", linkedlist.size());
