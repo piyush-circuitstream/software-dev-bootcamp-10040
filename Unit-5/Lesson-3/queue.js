94569d1d3976582d2aa59726ea98942f65135d2a
piyush-circuitstream <piyush@circuitstream.ca>
2026-02-05 22:01:41 -0700

    Stack and Queue examples

--- Unit-5/Lesson-3/queue.js
+++ Unit-5/Lesson-3/queue.js
@@ -9,22 +9,67 @@ class Queue {
     constructor() {
         this.front = null;
         this.rear = null;
+        this.size = 0;
     }
 
     size() {
-        // to be implemented
+        return this.size;
+    }
+
+    clear() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+        console.log("Queue is cleared!");
     }
 
     isEmpty() {
-        // to be implemented
+        return this.size === 0;
     }
 
     enqueue(data) {
-        // to be implemented
+        const newNode = new Node(data);
+
+        if (this.rear === null) {
+            // If the queue is empty, both front and rear will point to the new node
+            this.front = this.rear = newNode;
+        } else {
+            // Otherwise, add it to the end of the queue
+            this.rear.next = newNode;
+            this.rear = newNode;
+        }
+        this.size++;
     }
 
     dequeue() {
-        // to be implemented
-    }
+        if (this.front === null) {
+            console.log("Queue is empty");
+            return null;
+        }
 
+        const dequeuedData = this.front.data;
+        this.front = this.front.next;
+
+        // If the queue becomes empty, set rear to null as well
+        if (this.front === null) {
+            this.rear = null;
+        }
+        this.size--;
+        return dequeuedData;
+    }
 }
+
+const queue = new Queue();
+
+// Enqueue values to the queue
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+
+console.log(queue.size);
+
+console.log(queue.dequeue());    // Output: 10 (removes the front element)
+console.log(queue.size);
+
+queue.clear();                  // Clears the queue
+console.log(queue.isEmpty());    // Output: true (queue is empty)
--- Unit-5/Lesson-3/stack.js
+++ Unit-5/Lesson-3/stack.js
@@ -42,21 +42,75 @@ class Stack {
 
     // Add element to the top of stack
     push(data) {
-        // to be implemented
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
     }
 
     // Remove element from the top of stack
     pop() {
-        // to be implemented
+        if (!this.head) return null;
+
+        const poppedValue = this.head.data;
+        this.head = this.head.next;
+        return poppedValue;
     }
 
     // Remove element from the bottom of stack
     shift() {
-        // to be implemented
+        if (!this.head) return null;
+
+        let pointer = this.head;
+        if (!pointer.next) { // Only one element in stack
+            const value = pointer.data;
+            this.top = null;
+            return value;
+        }
+
+        // Traverse to the second-to-last node
+        while (pointer.next && pointer.next.next) {
+            pointer = pointer.next;
+        }
+
+        const value = pointer.next.data;
+        pointer.next = null;
+        this.length--;
+        return value;
     }
 
     // Add element at the bottom of stack
     unshift(data) {
-        // to be implemented
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let pointer = this.head;
+            while (pointer.next) {
+                pointer = pointer.next;
+            }
+            pointer.next = newNode;
+        }
+        this.length++;
     }
 }
+
+const stack = new Stack();
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+console.log(stack.size());  // Output: 3
+console.log(stack.getFirst());  // Output: 30 (top of the stack)
+console.log(stack.getLast());  // Output: 10 (bottom of the stack)
+
+console.log(stack.pop());  // Output: 30 (removes top of stack)
+console.log(stack.size());  // Output: 2
+
+stack.unshift(5);  // Add to the bottom of the stack
+console.log(stack.getLast());  // Output: 5 (new bottom)
+
+console.log(stack.shift());  // Output: 5 (removes from bottom of stack)
+console.log(stack.size());  // Output: 2
+
+stack.clear();  // Clear the stack
+console.log(stack.size());  // Output: 0
